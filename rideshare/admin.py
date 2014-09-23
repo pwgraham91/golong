79803e521258278301372f0d28504cb4e5b53850
Peter <pwgraham91@gmail.com>
2014-09-23 15:53:01 -0700

    950

--- rideshare/admin.py
+++ rideshare/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
 from rideshare.models import Traveler, Car, Route
 
-admin.site.register(Traveler)
+admin.site.register(Traveler, UserAdmin)
 admin.site.register(Car)
 admin.site.register(Route)
 
